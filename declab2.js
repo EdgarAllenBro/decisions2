594090bbeb5c955618ef89bbac3ae2a26b0d19e8
Cameron Grieder <cjgrieder@gmail.com>
2021-12-02 11:44:41 -0700

    solved problem 8

--- declab2.js
+++ declab2.js
@@ -52,7 +52,7 @@ for (let i = 0; i < nums.length; i++) {
     evensArr.push(nums[i])
   }
 }
-
+console.log(evensArr)
 
 ////////// PROBLEM 5 //////////
 
@@ -115,8 +115,10 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
-
+for (let i = 0; i < listOfNumbers.length; i++){
+if (listOfNumbers[i] % 3 == 0)
+console.log (`${listOfNumbers[i]} is divisible by 3`)
+}
 ////////// PROBLEM 9 //////////
 // Do not edit the code below.
 var letters = ['A', 'B', 'C', 'D', 'E'];
